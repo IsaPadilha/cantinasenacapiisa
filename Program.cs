8b8113819563d8422efb9b8843d796e59caf5b16
IsaPadilha <isap4619@gmail.com>
2025-11-28 20:24:58 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,64 +1,44 @@
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-var feedbackGroup = app.MapGroup("/feedbacks");
-var alunosGroup = app.MapGroup("/alunos");
-
-alunosGroup.MapGet("", () =>
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
 {
-    List<Aluno> alunos;
-    alunos = new AlunoController().ListarAlunos();
-    return Results.Ok(alunos);
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-alunosGroup.MapPost("", ([FromBody] Aluno aluno) =>
+app.UseHttpsRedirection();
+
+var summaries = new[]
 {
-    new AlunoController().PostarAluno(aluno);
-    return Results.Ok("Aluno adicionado com sucesso");
-});
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-alunosGroup.MapPut("", ([FromBody] Aluno aluno) =>
+app.MapGet("/weatherforecast", () =>
 {
-    new AlunoController().AtualizarAlunos(aluno);
-    return Results.Ok("ALuno atualizado com sucesso");
-});
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
 
-alunosGroup.MapDelete("", ([FromBody] Aluno aluno) =>
-{
-    new AlunoController().ExcluirAluno(aluno);
-    return Results.Ok("Aluno excluído com sucesso");
-});
-
-
-
-feedbackGroup.MapGet("", () =>
-{
-    List<Feedback> feedbacks;
-    feedbacks = new FeedbackController().ListarFeedbacks();
-    return Results.Ok(feedbacks);
-});
-
-feedbackGroup.MapPost("", ([FromBody] Feedback feedback) =>
-{
-    new FeedbackController().PostarFeedback(feedback);
-    return Results.Ok("Feedback adicionado com sucesso");
-});
-
-feedbackGroup.MapPut("", ([FromBody] Feedback feedback) =>
-{
-    new FeedbackController().AtualizarFeedback(feedback);
-    return Results.Ok("Feedback atualizado com sucesso");
-});
-
-feedbackGroup.MapDelete("", ([FromBody] Feedback feedback) =>
-{
-    if (feedback.UsuarioId != 1)
-    {
-        return Results.Forbid();
-    }
-        new FeedbackController().ExcluirFeedback(feedback);
-        return Results.Ok("Feedback excluído com sucesso");
-});
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
